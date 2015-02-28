bd4d4afba2d24a5d35810f321b903446550b2541
Rubén Aguilera Díaz-Heredero <raguilera@autentia.com>
2015-02-28 21:06:42 +0100

    Añadimos las tres pestañas

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,14 +1,7 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
 angular.module('starter', ['ionic'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
     if(window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
@@ -17,3 +10,41 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider.state('tab', {
+      url: '/tab',
+      abstract: true,
+      templateUrl: 'templates/tabs.html'
+    })
+
+    .state('tab.camera', {
+      url: '/camera',
+      views: {
+        'tab-camera': {
+          templateUrl: 'templates/tab-camera.html',
+        }
+      }
+    })
+    
+    .state('tab.contacts', {
+    		url: '/contacts',
+    		views: {
+    			'tab-contacts': {
+    				templateUrl: 'templates/tab-contacts.html'
+    			}
+    		}
+    })
+
+    .state('tab.rate', {
+      url: '/rate',
+      views: {
+        'tab-rate': {
+          templateUrl: 'templates/tab-rate.html'
+        }
+      }
+    })
+
+  $urlRouterProvider.otherwise('/tab/camera');
+
+});
